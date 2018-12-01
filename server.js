5dd99afa5b1af614f554017859f1751714f0bee8
dinopuguh <puguhmunung16@gmail.com>
2018-12-01 10:04:05 +0700

    use cors

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
+
+// var fs = require('fs')
+// var https = require('https')
 
 const potNodes = require('./routes/api/v1/pot-nodes');
 const soilTemperature = require('./routes/api/v1/soil-temperatures');
@@ -10,6 +14,7 @@ const app = express();
 
 //BodyParser Middleware
 app.use(bodyParser.json());
+app.use(cors());
 
 //DB config
 const db = require('./config/keys').mongoURI;
@@ -27,5 +32,13 @@ app.use('/api/v1/soil/temperatures', soilTemperature);
 app.use('/api/v1/soil/moistures', soilMoisture);
 
 const port = process.env.PORT || 5000;
+
+// https.createServer({
+//     key: fs.readFileSync('server.key'),
+//     cert: fs.readFileSync('server.cert')
+//   }, app)
+//   .listen(3000, function () {
+//     console.log(`Server started on port ${port}`);
+//   })
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
