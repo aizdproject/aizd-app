f3eb845d56c332d7b9db58b073e3c2ee78c57882
dinopuguh <puguhmunung16@gmail.com>
2019-02-19 10:07:24 +0700

    initial commit MERN

--- routes/api/v1/auth/roles.js
+++ routes/api/v1/auth/roles.js
@@ -1,40 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bodyParser = require('body-parser');
-router.use(bodyParser.urlencoded({
+const bodyParser = require("body-parser");
+router.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 
-const Role = require('../../../../models/Auth/Role');
+const Role = require("../../../../models/Auth/Role");
 
-router.get('/', (req, res) => {
-    Role.find()
-        .then(roles => res.json(roles));
+router.get("/", (req, res) => {
+    Role.find().then(roles => res.json(roles));
 });
 
-router.get('/:id', (req, res) => {
-    Role.findById(req.params.id)
-        .then(role => res.json(role));
+router.get("/:id", (req, res) => {
+  Role.findById(req.params.id).then(role => res.json(role));
 });
 
-router.post('/', (req, res) => {
-    const newRole = new Role({
-        name: req.body.name
-    });
+router.post("/", (req, res) => {
+  const newRole = new Role({
+    name: req.body.name
+  });
 
-    newRole.save().then(role => res.json(role));
-})
+  newRole.save().then(role => res.json(role));
+});
 
-router.delete('/:id', (req, res) => {
-    Role.findById(req.params.id)
-        .then(role => role.remove()
-            .then(() => res.json({
-                message: 'Berhasil menghapus role'
-            })))
-        .catch(err => res.status(404).json({
-            message: 'Gagal menghapus role',
-            error: err.message
-        }))
-})
+router.delete("/:id", (req, res) => {
+  Role.findById(req.params.id)
+    .then(role =>
+      role.remove().then(() =>
+        res.json({
+          message: "Berhasil menghapus role"
+        })
+      )
+    )
+    .catch(err =>
+      res.status(404).json({
+        message: "Gagal menghapus role",
+        error: err.message
+      })
+    );
+});
 
 module.exports = router;
