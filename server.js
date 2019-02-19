8c6c337aed7aa8f2fadf1d2ce8b8e07c89656df4
dinopuguh <puguhmunung16@gmail.com>
2019-02-19 10:59:45 +0700

    add proxy to client

--- server.js
+++ server.js
@@ -56,6 +56,15 @@ app.use("/api/v1/node-statistics", nodeStatistic);
 app.use("/api/v1/alpha", alphaService);
 app.use("/api/v1/outdoor", outdoorService);
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
