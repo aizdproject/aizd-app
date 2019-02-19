42956b6c1705a79f4cdcfb6e4e4dd6bb0de73807
dinopuguh <puguhmunung16@gmail.com>
2019-02-19 12:06:54 +0700

    change to frontend

--- server.js
+++ server.js
@@ -58,10 +58,10 @@ app.use("/api/v1/outdoor", outdoorService);
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('frontend/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
 
